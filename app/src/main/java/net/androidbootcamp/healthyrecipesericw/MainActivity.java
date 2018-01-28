7291fa2bbecb20d2bda7381b7a561ce80de2cefb
Eric Witowski <edubwitowski@gmail.com>
2018-01-28 09:53:34 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/healthyrecipesericw/MainActivity.java
@@ -0,0 +1,27 @@
+package net.androidbootcamp.healthyrecipesericw;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btnRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RecipeActivity.class));
+            }
+        });
+        {
+        }
+    }
+}
---
+++ app/src/main/java/net/androidbootcamp/healthyrecipesericw/RecipeActivity.java
@@ -0,0 +1,17 @@
+package net.androidbootcamp.healthyrecipesericw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        /*this is a comment*/
+        /**
+         * this is a comment also
+         */
+    }
+}
